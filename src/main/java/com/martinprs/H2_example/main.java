0825716880d96f6a641d71065f555501e10c4294
Martin Pruus <martinprs@users.noreply.github.com>
2026-06-03 02:18:13 +0300

    Rename main file & remove test

---
+++ src/main/java/com/martinprs/H2_example/main.java
@@ -0,0 +1,13 @@
+package com.martinprs.H2_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(main.class, args);
+	}
+
+}
